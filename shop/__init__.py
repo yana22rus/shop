5c6fc956a6052696143ace9bde878a22f91298fb
qwe <test@mail.ru>
2023-02-26 19:35:51 +0300

    chage scheme

--- shop/__init__.py
+++ shop/__init__.py
@@ -14,7 +14,7 @@ migrate = Migrate(app, db)
 class Goods(db.Model):
     __tablename__ = 'goods'
     id = db.Column(db.Integer(), primary_key=True)
-    price = db.Column(db.Integer(), nullable=False)
+    price = db.Column(db.Integer, db.CheckConstraint('number_stock>=1'), nullable=False)
     title = db.Column(db.String(255), nullable=False, unique=True)
     subtitle = db.Column(db.String(255), nullable=True)
     description = db.Column(db.String(2000), nullable=True)
@@ -22,7 +22,7 @@ class Goods(db.Model):
     category = db.Column(db.String(255), nullable=False)
     brand = db.Column(db.String(255), nullable=False)
     color = db.Column(db.String(255), nullable=False)
-    number_stock = db.Column(db.Integer, db.CheckConstraint('number_stock>=1'), nullable=False)
+    number_stock = db.Column(db.Integer, db.CheckConstraint('number_stock>=0'), nullable=False)
     created_on = db.Column(db.DateTime, default=datetime.now)
     updated_on = db.Column(db.DateTime, default=datetime.now, onupdate=datetime.now)
 
