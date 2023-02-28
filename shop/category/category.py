25db99bafbd7b3fa9d9545d2934de6d1ab1f4fb6
qwe <test@mail.ru>
2023-02-28 23:07:11 +0300

    add dow file

--- shop/category/category.py
+++ shop/category/category.py
@@ -1,17 +1,32 @@
+from uuid import uuid4
+import os
 from flask import Blueprint, render_template, request
 from shop import db, Category
-
-
-
 
 category_bp = Blueprint('category', __name__, template_folder='templates', static_folder='static',
                         static_url_path='/static/category')
 
+ALLOWED_EXTENSIONS = {"png", "jpg", "jpeg"}
 
-@category_bp.route("/create_category")
+UPLOAD_FOLDER = os.path.join("img", "uploads")
+
+
+@category_bp.route("/create_category", methods=["GET", "POST"])
 def create():
-    # create_news = Category(title="tes1117t123",img="Sasha Grey")
-    # db.session.add(create_news)
-    # db.session.flush()
-    # db.session.commit()
+    if request.method == "POST":
+        title = request.form["title"]
+
+        file = request.files["file"]
+
+        file_extensions = file.filename
+
+        file.filename = f'{uuid4()}.{file.filename.split(".")[-1].lower()}'
+
+        file.save(os.path.join("static", UPLOAD_FOLDER, file.filename))
+
+        create_news = Category(title=title, img=file.filename)
+        db.session.add(create_news)
+        db.session.flush()
+        db.session.commit()
+
     return render_template("create_category.html")
--- shop/config.py
+++ shop/config.py
@@ -1,5 +1,10 @@
+import os
+
+UPLOAD_FOLDER = os.path.join("img", "uploads")
+
 class Config(object):
     SECRET_KEY = "qwe"
     SQLALCHEMY_DATABASE_URI = "sqlite:///app.db"
     SQLALCHEMY_TRACK_MODIFICATIONS = True
+    UPLOAD_FOLDER = UPLOAD_FOLDER
     DEBUG = True
